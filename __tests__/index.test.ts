fcb0e606f19e877ab868e81061702458a6fe863a
Danny Koppenhagen <mail@k9n.dev>
2023-11-27 08:33:53 +0100

    chore: modernize whole setup
    used typescript-action template from:
    https://github.com/actions/typescript-action/tree/main

--- __tests__/index.test.ts
+++ __tests__/index.test.ts
@@ -1,112 +1,115 @@
-import * as core from '@actions/core';
-import * as exec from '@actions/exec';
-import * as github from '@actions/github';
-import * as io from '@actions/io';
-import * as path from 'path';
+import * as core from '@actions/core'
+import * as exec from '@actions/exec'
+import * as github from '@actions/github'
+import * as io from '@actions/io'
+import * as path from 'path'
 
-import run from '../index';
+import { run } from '../src/main'
 
-const originalContext = { ...github.context };
-const originalGitHubWorkspace = process.env['GITHUB_WORKSPACE'];
-const gitHubWorkspace = path.resolve('/checkout-tests/workspace');
+const originalContext = { ...github.context }
+const originalGitHubWorkspace = process.env['GITHUB_WORKSPACE']
+const gitHubWorkspace = path.resolve('/checkout-tests/workspace')
 
 type InputsMock = {
-  [name: string]: string;
-};
+  [name: string]: string
+}
 
-let inputs: InputsMock = {};
-let execSpy: jest.SpyInstance;
+let inputs: InputsMock = {}
+let execSpy: jest.SpyInstance
 
 beforeAll(() => {
-  execSpy = jest.spyOn(exec, 'exec').mockImplementation(jest.fn());
-  jest.spyOn(io, 'cp').mockImplementation(jest.fn());
+  execSpy = jest.spyOn(exec, 'exec').mockImplementation(jest.fn())
+  jest.spyOn(io, 'cp').mockImplementation(jest.fn())
 
   jest.spyOn(core, 'getInput').mockImplementation((name: string) => {
-    return inputs[name];
-  });
+    return inputs[name]
+  })
 
   jest.spyOn(github.context, 'repo', 'get').mockImplementation(() => {
     return {
       owner: 'foo',
-      repo: 'foo.github.io',
-    };
-  });
+      repo: 'foo.github.io'
+    }
+  })
 
-  github.context.ref = 'refs/heads/some-ref';
-  github.context.sha = '1234567890123456789012345678901234567890';
+  github.context.ref = 'refs/heads/some-ref'
+  github.context.sha = '1234567890123456789012345678901234567890'
 
-  process.env['GITHUB_WORKSPACE'] = gitHubWorkspace;
-});
+  process.env['GITHUB_WORKSPACE'] = gitHubWorkspace
+})
 
 afterAll(() => {
-  delete process.env['GITHUB_WORKSPACE'];
+  delete process.env['GITHUB_WORKSPACE']
   if (originalGitHubWorkspace) {
-    process.env['GITHUB_WORKSPACE'] = originalGitHubWorkspace;
+    process.env['GITHUB_WORKSPACE'] = originalGitHubWorkspace
   }
 
-  github.context.ref = originalContext.ref;
-  github.context.sha = originalContext.sha;
+  github.context.ref = originalContext.ref
+  github.context.sha = originalContext.sha
 
-  jest.restoreAllMocks();
-});
+  jest.restoreAllMocks()
+})
 
 beforeEach(() => {
-  jest.resetModules();
+  jest.resetModules()
   inputs = {
-    'access-token': 'SECRET',
-  };
-});
+    'access-token': 'SECRET'
+  }
+})
 
 describe('scully Publish action', () => {
   it('returns an error when no access token is given', async () => {
-    inputs['access-token'] = '';
-    const setFailedSpy = jest.spyOn(core, 'setFailed');
+    inputs['access-token'] = ''
+    const setFailedSpy = jest.spyOn(core, 'setFailed')
 
-    await run();
+    await run()
 
-    expect(setFailedSpy).toBeCalledWith(
-      'No personal access token found. Please provide one by setting the `access-token` input for this action.',
-    );
-  });
+    expect(setFailedSpy).toHaveBeenCalledWith(
+      'No personal access token found. Please provide one by setting the `access-token` input for this action.'
+    )
+  })
 
   it('calls the install script without additional args', async () => {
-    inputs['install-args'] = '';
+    inputs['install-args'] = ''
 
-    await run();
+    await run()
 
-    expect(execSpy).toHaveBeenLastCalledWith('yarn install --frozen-lockfile');
-  });
+    expect(execSpy).toHaveBeenCalledWith('npm ci')
+  })
 
   it('calls the install script with additional args', async () => {
-    inputs['install-args'] = '--force';
+    inputs['install-args'] = '--force'
 
-    await run();
+    await run()
 
-    expect(execSpy).toHaveBeenLastCalledWith('yarn install --frozen-lockfile --force');
-  });
+    expect(execSpy).toHaveBeenCalledWith('npm ci --force')
+  })
 
   it('skips if deploy branch is the same as the current git head', async () => {
-    inputs['deploy-branch'] = 'some-ref';
-    github.context.ref = 'refs/heads/some-ref';
+    inputs['deploy-branch'] = 'some-ref'
+    github.context.ref = 'refs/heads/some-ref'
 
-    await expect(run()).resolves.not.toThrowError();
-  });
+    await expect(run()).resolves.not.toThrow()
+  })
 
   it('calls angular build without args', async () => {
-    inputs['build-args'] = '';
-    inputs['scully-args'] = '';
+    inputs['build-args'] = ''
+    inputs['scully-args'] = ''
 
-    await run();
+    await run()
 
-    expect(execSpy).toHaveBeenLastCalledWith('yarn run build', []);
-  });
+    expect(execSpy).toHaveBeenCalledWith('npm run build', [])
+  })
 
   it('calls angular build with args', async () => {
-    inputs['build-args'] = '--prefix-paths --no-uglify';
-    inputs['scully-args'] = '';
+    inputs['build-args'] = '--prefix-paths --no-uglify'
+    inputs['scully-args'] = ''
 
-    await run();
+    await run()
 
-    expect(execSpy).toHaveBeenLastCalledWith('yarn run build -- --prefix-paths --no-uglify', []);
-  });
-});
+    expect(execSpy).toHaveBeenCalledWith(
+      'npm run build -- --prefix-paths --no-uglify',
+      []
+    )
+  })
+})
